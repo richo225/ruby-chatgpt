0161277e17a1f4a22b4b3072ed4a5d4d61a156d8
Richard Bates <rich.bates@protonmail.com>
2023-05-06 23:52:26 +0100

    Request ChatGPT content and parse response

--- lib/ruby/chatgpt.rb
+++ lib/ruby/chatgpt.rb
@@ -1,10 +1,30 @@
 # frozen_string_literal: true
 
 require_relative "chatgpt/version"
+require "openai"
 
 module Ruby
   module Chatgpt
-    class Error < StandardError; end
-    # Your code goes here...
+    class MissingApiKeyError < StandardError; end
+
+    unless ENV["OPENAI_API_KEY"]
+      raise MissingApiKeyError,
+            "Please set a value for the OPENAI_API_KEY variable"
+    end
+
+    client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"])
+
+    print "Enter a prompt: "
+    input = gets.chomp
+
+    response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo", # Required.
+        messages: [{ role: "user", content: input }], # Required.
+        temperature: 0.7
+      }
+    )
+
+    puts response["choices"][0]["message"]["content"]
   end
 end
